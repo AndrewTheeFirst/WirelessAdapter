5951c7b51b434604d23fb76c5985efbb5f81e1c4
AndrewTheeFirst <03drewp@gmail.com>
2026-05-02 20:20:57 -0400

    mouse_task rework to accumulate deltas for mouse messages waiting in queue. * Eliminates mouse floating over high polling rate mice. * Increases mouse accuracy and latency

--- wireless_receiver-2.0/main/devices/mouse.c
+++ wireless_receiver-2.0/main/devices/mouse.c
@@ -6,13 +6,15 @@
 #include "tusb_device_common.h"
 #include "tusb.h"
 
-#define MOUSE_QUEUE_SIZE 100
+#define MOUSE_QUEUE_SIZE 20
 
 // static const char* TAG = "USB_TRANSMITTER // mouse.c";
 
 static QueueHandle_t mouse_queue = NULL;
 
 static TaskHandle_t mouse_task_handle = NULL;
+
+static inline int8_t clamp16to8(int16_t val) { return (val < -128) ? -128 : ((val > 127) ? 127 : val); }
 
 static bool __send_report(espnow_msg_mouse_t* msg){
     return tud_hid_n_mouse_report(
@@ -27,25 +29,41 @@ static bool __send_report(espnow_msg_mouse_t* msg){
 }
 
 static void mouse_task(void* arg){
-    espnow_msg_mouse_t mouse_msg_buf;
+    espnow_msg_mouse_t mouse_msg_buf, mouse_msg_acc;
+    
     while (true){
+        int16_t x = 0, y = 0, wheel = 0, pan = 0;
         // attempt to retrieve msg from queue
         // restart on failure
         if (xQueueReceive(mouse_queue, &mouse_msg_buf, portMAX_DELAY) != pdTRUE)
             continue;
-
+        
+        // accumulate dx, dy, wheel, and pan
+        while (xQueueReceive(mouse_queue, &mouse_msg_acc, 0) == pdTRUE){
+            x += mouse_msg_acc.x;
+            y += mouse_msg_acc.y;
+            wheel += mouse_msg_acc.wheel;
+            pan += mouse_msg_acc.pan;
+        }
+        mouse_msg_buf.x = clamp16to8(mouse_msg_buf.x + x);
+        mouse_msg_buf.y = clamp16to8(mouse_msg_buf.y + y);
+        mouse_msg_buf.wheel = clamp16to8(mouse_msg_buf.wheel + wheel);
+        mouse_msg_buf.pan = clamp16to8(mouse_msg_buf.pan + pan);
+        
         // wait for the interface to become ready
         int num_tries = 0;
         while (!tud_hid_n_ready(HID_MOUSE_INSTANCE) && (num_tries++ < 5)) {
             // Sleep until USB callback wakes us (or give up after 10MS)
             ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(10));
+            
             // Check if still mounted after waiting
             if (!tud_mounted()) {
                 break;
             }
         }
 
-        if (num_tries < 5) { __send_report(&mouse_msg_buf); }
+        if (tud_mounted() && tud_hid_n_ready(HID_MOUSE_INSTANCE))
+            __send_report(&mouse_msg_buf);
     }
 }
 
